7c8ccc01a50e4dc1fb1b23da9486f7a9b135d00d
gaopeng527 <1259728004@qq.com>
2016-08-14 11:05:45 +0800

    内容服务

--- zhonghui-manager-service/src/main/java/com/zhonghui/service/impl/ContentServiceImpl.java
+++ zhonghui-manager-service/src/main/java/com/zhonghui/service/impl/ContentServiceImpl.java
@@ -3,11 +3,13 @@ package com.zhonghui.service.impl;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.huizhong.mapper.TbContentMapper;
 import com.huizhong.pojo.TbContent;
 import com.zhonghui.common.pojo.ZhonghuiResult;
+import com.zhonghui.common.utils.HttpClientUtil;
 import com.zhonghui.service.ContentService;
 /**
  * 内容管理Service
@@ -19,6 +21,10 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
 	
 	@Override
 	public ZhonghuiResult insertContent(TbContent content) {
@@ -26,6 +32,15 @@ public class ContentServiceImpl implements ContentService {
 		content.setCreated(new Date());
 		content.setUpdated(new Date());
 		contentMapper.insert(content);
+		
+		// 添加缓存同步逻辑
+		try {
+			HttpClientUtil.doGet(REST_BASE_URL+REST_CONTENT_SYNC_URL+content.getCategoryId());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return ZhonghuiResult.ok();
 	}
 	
